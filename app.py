8308fb1e4461bac78e1eb16718a2cccb4e536101
guoxx663 <guoxx663@umn.edu>
2018-11-10 16:13:32 -0500

    more add

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def homepage():
 def register():
       return render_template("register.html")
 
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
