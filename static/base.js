86d1043b10df10c45bfcac5e2e29b332592e05f6
willzma <willzma15@gmail.com>
2018-11-24 08:27:18 -0500

    Fixed tab highlighting

--- static/base.js
+++ static/base.js
@@ -13,7 +13,7 @@ function openTab(evt, tabName) {
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
