4611bb69f43064c4be04a3eb7eb7dc278e1e883f
willzma <willzma15@gmail.com>
2018-11-25 08:37:33 -0500

    Most of course selection done

---
+++ split_courses_by_abbr.py
@@ -0,0 +1,28 @@
+from firebase_admin import credentials
+from firebase_admin import db
+import firebase_admin
+import json
+
+if __name__ == '__main__':
+    cred = credentials.Certificate('buzzplan-d333f-firebase-adminsdk-jhebg-c6fbec263e.json')
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://buzzplan-d333f.firebaseio.com/'
+    })
+    ref = db.reference()
+    courses_ref = ref.child('Courses')
+    courses_by_abbr_ref = ref.child('courses_by_abbr')
+    courses = courses_ref.get()
+    courses_by_abbr_dict = {}
+    for key in courses:
+        cutoff_index = None
+        for i in range(len(key)):
+            if key[i].isnumeric():
+                cutoff_index = i
+                break
+        abbr = key[:cutoff_index - 1].strip()
+        code = key[cutoff_index:].strip()
+        if abbr not in courses_by_abbr_dict:
+            courses_by_abbr_dict[abbr] = {}
+        if 'X' not in code:
+            courses_by_abbr_dict[abbr][key] = courses[key]
+    courses_by_abbr_ref.set(courses_by_abbr_dict)
--- static/base.js
+++ static/base.js
@@ -1,7 +1,3 @@
-var CALENDAR_START = 480 // 8:00 AM in minutes
-var CALENDAR_END = 1320 // 10:00 PM in minutes
-var CALENDAR_RANGE = CALENDAR_END - CALENDAR_START
-
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -23,45 +19,6 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-
-// Tue: 9:15 - 11:15   startTime = ((9 * 60) + 15)
-// addClass(2, 60, 75, name, location)
-/**
- * Adds a course bubble to the calendar (visually).
- * 
- * @param {*} code - abbreviation and course number; e.g. 'CS 6400'
- * @param {*} day - numeric day from Monday - Friday, 0 - 4 respectively
- * @param {*} startTime - the start time of the course in minutes
- * @param {*} duration - duration of the course in minutes
- * @param {*} location - location where the course is being held
- */
-function addClass(code, day, startTime, duration, location) {
-    var div1 = document.createElement('div');
-    var div2 = document.createElement('div');
-    var div3 = document.createElement('div');
-    var div4 = document.createElement('div');
-    div1.className = 'course-box';
-    div2.className = 'course-cal pinned';
-    div3.className = 'course-content';
-    div4.className = 'location';
-
-    var startPosition = 100 * (startTime - CALENDAR_START) / CALENDAR_RANGE;
-    var height = 100 * duration / CALENDAR_RANGE;
-
-    div1.style.top = startPosition + '%';
-    div1.style.height = height + '%';
-    div2.style.backgroundColor = 'red';
-    div2.style.borderColor = 'red';
-
-    div3.innerText = code;
-    div4.innerText = location;
-
-    div2.appendChild(div3);
-    div2.appendChild(div4);
-    div1.appendChild(div2);
-
-    document.getElementsByClassName('week-day-body-col')[day].appendChild(div1);
-}
 function testLoad(){
     console.log(window.userData);
 }
