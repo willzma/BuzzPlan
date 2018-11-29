bc43058654dd746540da01ccc2822d939dcdeddf
guoxx663 <guoxx663@umn.edu>
2018-11-29 10:49:12 -0500

    remove scrape.py

--- scrape.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import html5lib
-import json
-
-df = pd.read_html("https://critique.gatech.edu/course.php?id=MATH3022", header=0, parse_dates=["GPA"])
-
-print(df[0].to_json(orient='split'))
